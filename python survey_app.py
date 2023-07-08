7cb56c8994bdcc9785ef273f9dfa989bef50324e
Popeydanielmate <14269@coderacademy.edu.au>
2023-07-08 18:00:24 +1000

    third commit

--- python survey_app.py
+++ python survey_app.py
@@ -4,4 +4,13 @@ class Customer:
         self.name = name
         self.responses = {} # Dictionary to be used to store survey responses for each question
         
-    
+    def collect_survey_response(self, question, answer):
+        self.responses[question] = answer
+        
+    def validate_responses(self):
+        # Validate the survey responses (e.g., check if all questions were answered)
+        # Return True if responses are valid, False otherwise
+        if len(self.responses) == 2:  
+            return True
+        else:
+            return False    
